827df77d7158d6e184ef7042670a0667e072b7b8
Matt Habbo <matthabbo45@gmail.com>
2022-06-12 17:04:18 -0400

    pwa manifest

--- service-worker.js
+++ service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('install', function (e) {
 });
 
 self.addEventListener('activate', function (e) {
-    e.waituntil(
+    e.waitUntil(
         caches.keys().then(function (keyList) {
             let cacheKeepList = keyList.filter(function (key) {
                 return key.indexOf(APP_PREFIX);
